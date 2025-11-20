c1481a134e449da97ac65038dfd2a578202b0c7f
matheuzuilian <matheus.willian11@gmail.com>
2025-11-19 22:08:08 -0400

    Adicionado SSL

--- src/db.js
+++ src/db.js
@@ -7,7 +7,11 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // <--- Garanta que a porta está aqui
+    ssl: {
+        rejectUnauthorized: false // <--- AQUI ESTÁ O SEGREDO!
+    }
 });
 
 // Tenta conectar
